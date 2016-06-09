59cfe612e47101e93d71266857ec431c5d61556f
Mara Wanot <m.d.wanot@hotmail.com>
2016-06-09 10:20:46 +0100

    addd rubocop

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Receive an event", :type => :request do
+RSpec.describe "Receive an event", type: :request do
 
   it "receives an event in json format" do
     headers = { "CONTENT_TYPE" => "text/json" }
@@ -9,4 +9,3 @@ RSpec.describe "Receive an event", :type => :request do
   end
 
 end
-
